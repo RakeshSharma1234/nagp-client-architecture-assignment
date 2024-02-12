d60b564c4f7c69a7b8fd52ed84e4b699d13e464a
Rakesh Sharma <rakesh.sharma04@nagarro.com>
2024-02-12 17:48:42 +0530

    Changes

--- projects/premium-mfe-app/src/app/premium/premium.component.ts
+++ projects/premium-mfe-app/src/app/premium/premium.component.ts
@@ -1,5 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { SharedLibService } from '../../../../shared-lib/src/public-api';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-premium',
@@ -7,18 +8,44 @@ import { SharedLibService } from '../../../../shared-lib/src/public-api';
   styleUrl: './premium.component.scss'
 })
 export class PremiumComponent implements OnInit {
-  constructor(private sharedLibService:SharedLibService){}
+  constructor(private sharedLibService:SharedLibService, private route: ActivatedRoute){}
   worker: Worker | undefined;
+  policyNumber:string | undefined;
+  policyPremium:string | undefined;
   ngOnInit(): void {
-     if (typeof Worker !== 'undefined') {
-      // Create a new
-      this.worker =  new Worker(new URL('../premium.worker', import.meta.url));
-      this.worker.onmessage = ({ data }) => {
-        alert(`page got message: ${data}`);
-      };
-      this.worker.postMessage(80);
-    } else {
-      alert("Web workers are not supported in this environment");
+    //  if (typeof Worker !== 'undefined') {
+    //   // Create a new
+    //   this.worker =  new Worker(new URL('../premium.worker', import.meta.url));
+    //   this.worker.onmessage = ({ data }) => {
+    //     alert(`page got message: ${data}`);
+    //   };
+    //   this.worker.postMessage(80);
+    // } else {
+    //   alert("Web workers are not supported in this environment");
+    // }
+    this.route.queryParams.subscribe(params => {
+      this.policyNumber = params['policy'];
+      this.setPolicyPremium();
+    });
+  }
+  
+  setPolicyPremium():void {
+    if(this.policyNumber && this.policyNumber.length > 0){
+      if(this.sharedLibService.getPolicy(this.policyNumber)){
+       this.policyPremium = this.sharedLibService.getPolicy(this.policyNumber)['premium'];
+      }
+    } else{
+      this.policyPremium = '';
+    }
+  }
+
+  payment():void {
+    if(!this.policyNumber || !this.policyPremium){
+      alert("Please provide the policyNumber and Premium");
+      return;
+    }
+    if(this.policyNumber.length > 0){
+       alert("Payment is completed for Policy: "+this.policyNumber+" with premium amount: "+this.policyPremium);
     }
   }
   
--- projects/shared-lib/src/lib/shared-lib.service.ts
+++ projects/shared-lib/src/lib/shared-lib.service.ts
@@ -15,15 +15,16 @@ export class SharedLibService {
     localStorage.setItem("insurancePolicies",JSON.stringify(insurance_policies));
   }
 
-  getPolicy(policyNumber:string) {
+  getPolicy(policyNumber:any) {
     let policyNo = policyNumber.toUpperCase()
     let localStoragePolicies:any = localStorage.getItem("insurancePolicies");
     let policies:any = JSON.parse(localStoragePolicies);
     const policy = policies.filter((policy:any)=> policy["policyNumber"] === policyNo);
-    if(policy){
+    if(policy[0]){
     return policy[0];
     }else{
       alert("No Policy Found for policyNumber: "+policyNumber);
+      return undefined;
     }
   }
 }
